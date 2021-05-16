33f9d4dae128556370f83da0c51cafb8b7cc9d9f
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2021-05-16 06:23:57 +0530

    Variables

---
+++ 09.Variables.lua
@@ -0,0 +1,9 @@
+print(type("Type of Variables"))    
+print("10 =", type(10))     
+print("10.5 =", type(10.5))        
+print("True =", type(true))            
+print("Print = ",type(print))           
+print( "NIL =", type(nil))   
+x= "ABC"
+print("ABC = ",type(x))           
+print("ABC = ",type(type(ABC)))        
